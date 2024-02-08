e6e0fb1ba18eaf2da2b01f25a94ba9d15de5d504
AresWare <49085485+AresWare@users.noreply.github.com>
2024-02-07 20:59:37 -0500

    Add files via upload

---
+++ Check and change groups to private.ps1
@@ -0,0 +1,24 @@
+﻿#Grabs all O365 Groups
+$Groups = get-unifiedgroup | Select-Object -ExpandProperty PrimarySmtpAddress
+#Creates a loops to check groups for access type
+Foreach ($Group in $Groups)
+{
+    #Grabs group email and checks it's access type
+    Write-Host "Checking $Group for Access Type" -ForegroundColor White
+    $AccessType = get-unifiedgroup -identity $Group | Select-Object -ExpandProperty AccessType
+
+    If ($AccessType -match "Public")
+    #Changes group to private if it's public
+    {
+    Write-Host "Changing $Group to Private" -ForegroundColor Red
+    Set-UnifiedGroup -Identity $Group -AccessType Private
+
+    }
+
+    Else
+    #Writes host the group is already private
+    {
+    Write-Host "Great! $Group is Private" -ForegroundColor Green
+    }
+
+}
